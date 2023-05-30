e2aabf14ba9b96c5dab74fde9e600a0704b03de0
santhosh-git-23 <129048391+santhosh-git-23@users.noreply.github.com>
2023-05-30 17:04:35 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/hello/HelloWorldApplication.java
+++ src/main/java/com/example/hello/HelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
